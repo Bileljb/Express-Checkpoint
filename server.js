8ca965cd1c308108c7372aabdee1ca0d35c9dac5
unknown <bileljabrane6@gmail.com>
2022-01-17 14:23:27 +0100

    express

---
+++ server.js
@@ -0,0 +1,35 @@
+
+const express = require('express')
+const { use } = require('express/lib/application')
+const app = express()
+
+const middleware = (req, res, next) => {
+
+
+    const d = new Date();
+    let day = d.getDay()
+  
+    let hour = d.getHours();
+
+
+        if  (( 1<=day <=5) && (15 <= hour <= 17))  { return res.sendFile(__dirname+'/public/Closed.html')}
+    
+
+    next()
+}
+app.use(middleware);
+// app.get('/', (req,res)=>{
+//     res.sendFile(__dirname +'/public/')
+
+// })
+// app.get('/style.css', (req,res)=>{
+//     res.sendFile(__dirname +'/public/style.css')
+
+// })
+ app.use(express.static('public'))
+
+
+const port = 3000
+
+app.listen(port, () => console.log(`port is running on ${port}`))
+
